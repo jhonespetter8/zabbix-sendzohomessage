9fb22866a7a8c83b37d3423ffee72422938b132c
jhonespetter <jhones.petter@gmail.com>
2020-03-19 21:44:46 +0000

    Update sendzohomessage.py

--- sendzohomessage.py
+++ sendzohomessage.py
@@ -5,15 +5,12 @@ Version: 1.0
 Create in: 19/03/2020
 Descripton: Submit message from script to plataform zoho cliq, in channel or bot.
 Requirements: requests,json,sys
-Documentation Zoho: https://help.zoho.com/portal/en/community/topic/cliq-bots-post-message-to-a%C2%A0bot-using-the-command-line
-Generate token: https://accounts.zoho.com/apiauthtoken/create?SCOPE=ZohoCliq/InternalAPI
-Generate message: https://cliq.zoho.com/messagebuilder
 
 """
 
 import requests,json,sys
 
-token="145c0e4eca9be181c9751a51af8b6de0"
+token="YOURTOKEN"
 sentby="Monitoramento Data Center"
 destination=sys.argv[1]
 subject=sys.argv[2]
