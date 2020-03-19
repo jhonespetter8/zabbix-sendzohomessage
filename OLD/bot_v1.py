1e6fc3fb0f4740643a914b1f0fc3f41de9d467f6
jhonespetter <jhones.petter@gmail.com>
2020-03-19 23:48:53 +0000

    Update bot_v1.py

--- OLD/bot_v1.py
+++ OLD/bot_v1.py
@@ -4,7 +4,7 @@
 import requests
 import json
 
-url = "https://cliq.zoho.com/api/v2/bots/zabbixdatacenter/message"
-headers = {'Content-type': 'application/json', 'Authorization': 'Zoho-authtoken 145c0e4eca9be181c9751a51af8b6de0'}
+url = "https://cliq.zoho.com/api/v2/bots/XXXX/message"
+headers = {'Content-type': 'application/json', 'Authorization': 'Zoho-authtoken XXXX'}
 data = {"text": "Texto enviado ao bot!", "broadcast" :"true", "card": {"title": "FALA JOVENSSS!", "thumbnail": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif", "theme": "prompt"}}
 r = requests.post(url, data=json.dumps(data), headers=headers)
